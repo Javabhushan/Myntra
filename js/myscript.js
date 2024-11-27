2c1b0aa690a24b243a5818b607e1e900d3d20509
Javabhushan <kulkarnibhushan444@gmail.com>
2024-11-27 14:36:01 +0530

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,46 @@
+
+//  Medal Worthy Brands To Bag Section Starts
+
+$('#medal').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+//  Medal Worthy Brands To Bag Section Ends
+
+
+
+// Grand Global Brands Section Starts
+
+$('#grand').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
